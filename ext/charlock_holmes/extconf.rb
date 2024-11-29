970da8ed7c6324e6bc182649d8b557f959ea3e3e
José C. Ruiz <jose.ruiz@nextail.co>
2024-11-29 11:51:30 +0100

    changes

--- ext/charlock_holmes/extconf.rb
+++ ext/charlock_holmes/extconf.rb
@@ -21,6 +21,18 @@ end
 # ICU dependency
 #
 
+def show_variables()
+  puts "Variables de entorno:"
+  ENV.each do |key, value|
+    puts "#{key} = #{value}"
+  end
+  puts "Variables de instancia:"
+  instance_variables.each do |var|
+    puts "#{var}: #{instance_variable_get(var)}"
+  end
+end
+
+puts("paso 1")
 dir_config 'icu'
 
 rubyopt = ENV.delete("RUBYOPT")
@@ -28,20 +40,25 @@ rubyopt = ENV.delete("RUBYOPT")
 icuconfig = ""
 icu4c = "/usr"
 # detect homebrew installs
+
+puts("paso 2")
 if !have_library 'icui18n'
+  puts("paso 3")
   base = if !`which brew`.empty?
     `brew --cellar`.strip
   elsif File.exists?("/usr/local/Cellar/icu4c")
     '/usr/local/Cellar'
   end
-
+  puts("paso 4")
+  show_variables()
   if base and icu4c = Dir[File.join(base, 'icu4c/*')].sort.last
+    puts("paso 5")
     $INCFLAGS << " -I#{icu4c}/include "
     $LDFLAGS  << " -L#{icu4c}/lib "
     icuconfig = "#{icu4c}/bin/icu-config"
   end
 end
-
+puts("paso 6")
 unless have_library 'icui18n' and have_header 'unicode/ucnv.h'
   STDERR.puts "\n\n"
   STDERR.puts "***************************************************************************************"
@@ -49,19 +66,19 @@ unless have_library 'icui18n' and have_header 'unicode/ucnv.h'
   STDERR.puts "***************************************************************************************"
   exit(1)
 end
-
+puts("paso 7")
 have_library 'z' or abort 'libz missing'
 have_library 'icuuc' or abort 'libicuuc missing'
 have_library 'icudata' or abort 'libicudata missing'
-
+puts("paso 8")
 # icu4c might be built in C++11 mode, but it also might not have been
 icuconfig = `which icu-config`.chomp if icuconfig.empty?
 if File.exist?(icuconfig) && `#{icuconfig} --cxxflags`.include?("c++11")
   $CXXFLAGS << ' -std=c++11'
 end
-
+puts("paso 9")
 $CFLAGS << ' -Wall -funroll-loops'
 $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
-
+puts("paso 10")
 ENV['RUBYOPT'] = rubyopt
 create_makefile 'charlock_holmes/charlock_holmes'
